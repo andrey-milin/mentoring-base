0837c465ef7fcdbf52353d928aac3daae915fe39
andrey-milin <andrey.milin.nur@gmail.com>
2025-08-30 06:41:42 +0500

    добавил диалогое окно в юзеры

--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -1,34 +1,52 @@
-import { NgIf } from "@angular/common";
-import { Component, EventEmitter, Output } from "@angular/core";
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from  '@angular/material/input' ;
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-    selector: 'app-create-form-user',
-    templateUrl: './create-user-form.component.html',
-    styleUrl: './create-user-form.component.scss',
-    standalone: true,
-    imports: [ReactiveFormsModule, NgIf, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule],
+  selector: 'app-create-form-user',
+  templateUrl: './create-user-form.component.html',
+  styleUrl: './create-user-form.component.scss',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+  ],
 })
+export class CreateUserFormComponent {
+  @Output()
+  createUser = new EventEmitter();
 
-export class CreateUserFormComponent{
-
-@Output ()
-createUser = new EventEmitter();
-
- public form = new FormGroup({
+  public form = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    address: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    companyName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
+    companyName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/), Validators.minLength(7)]),
- });
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d+$/),
+      Validators.minLength(7),
+    ]),
+  });
 
- public submintForm(): void{
+  public submintForm(): void {
     this.createUser.emit(this.form.value);
     this.form.reset();
-}
+  }
 }
--- src/app/users-list/user-card/user-card.component.ts
+++ src/app/users-list/user-card/user-card.component.ts
@@ -1,21 +1,34 @@
-import { Component, EventEmitter, Input, Output, } from "@angular/core";
+import { Component, EventEmitter, inject, Input, Output, } from "@angular/core";
 import { User } from "../user.interface";
+import { MatDialog } from "@angular/material/dialog";
+import { EditUserDialogComponent } from "../edit-user-dialog/edit-user-dialog.component";
 
 @Component({
-    selector: 'app-user-card',
-    standalone: true,
-    templateUrl: './user-card.component.html',
-    styleUrl: './user-card.component.scss',
+  selector: 'app-user-card',
+  standalone: true,
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.scss',
 })
-
 export class userCardComponent {
-    @Input()
-    user!: User;
+  @Input()
+  user!: User;
 
-    @Output()
-    deleteUser = new EventEmitter<number>()
+  @Output()
+  deleteUser = new EventEmitter<number>();
 
-    onDeleteUser(userId: number) {
-        this.deleteUser.emit(userId)
-    }
+  readonly dialog = inject(MatDialog);
+
+   openDialog(): void {
+    const dialogRef = this.dialog.open(EditUserDialogComponent, {
+      data: {test: '123'},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('МОДАЛКА ЗАКРЫЛАСЬ');
+    });
+}
+
+  onDeleteUser(userId: number) {
+    this.deleteUser.emit(userId);
+  }
 }
